b5dd1d4880537d5a676058cc3ab2810d46bc24d4
fivar-rox <72460532+fivar-rox@users.noreply.github.com>
2023-04-28 15:53:37 +0530

    changes

--- server.py
+++ server.py
@@ -105,5 +105,4 @@ def get_original():
                     {"name":"proportionality (rho)", "value":"1.01"}]
     
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+	app.run(debug=False)
